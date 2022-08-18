d88128e23f682c992ae6736e1dcb66f8c613558d
Drew <glavich_drew@yahoo.com>
2022-08-19 01:01:27 +0900

    implemented routing between container and marketing apps

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,15 +1,31 @@
 import React from "react";
 import reactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
-  reactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory ?? createMemoryHistory();
+
+  // onNavigate is not provided when run in isolation
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  reactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      if (history.location.pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
---
+++ questions.md
@@ -0,0 +1,2 @@
+1. Why should we only have one BrowserRouter and use MemoryRouters in all subapps?
+   - It's supposed to be to prevent situations where the two routers are trying to change the URL at the same time.
